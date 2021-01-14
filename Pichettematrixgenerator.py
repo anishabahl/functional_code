d4ad727eb1de353869a66dac053c6afa26fa5510
anishabahl <anisha.bahl@kcl.ac.uk>
2021-01-14 12:42:54 +0000

    Allows choice to model ideal band responses as Lorentzian

--- Pichettematrixgenerator.py
+++ Pichettematrixgenerator.py
@@ -2,6 +2,7 @@ import numpy as np
 from numpy import genfromtxt
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.colors
 import scipy
 from scipy import interpolate
 from scipy.interpolate import interp1d
@@ -9,7 +10,9 @@ from scipy.optimize import leastsq
 from scipy.optimize import least_squares
 from scipy.linalg import lstsq
 from scipy.optimize import fmin_slsqp
-from scipy.optimize import minimize 
+from scipy.optimize import minimize
+from scipy.sparse.linalg import lsmr
+from scipy.optimize import lsq_linear
 import sklearn
 from sklearn.linear_model import Ridge
 import math
@@ -27,14 +30,18 @@ band_parameters = 'idealbandparameters.csv'
 lightspectrum = 'IR light with LP UV filter thorugh exoscope and adaptor.txt'
 filterdata = 'AsahiSpectra_XVL0670.csv'
 spectrometer = 'spydercheckr_spectra_spectrometer'
-datapath = '/home/ab20/Data/System_Paper/Photonfocus/demosaiced/' #must have full hypercube data
+datapath = '/home/ab20/Data/System_Paper/Photonfocus/halfv2demosaiced/' #must have full hypercube data
 filetype = '.img'
 newname = 'Pichettematrix'
 newxname = 'fullxaxis'
 newpath = '/home/ab20/Data/Calibration_file/'
-prereorder = 'ON' #should be on for PF at the moment 
-lam = 0 #parameter for regularisation
-alpha = 1 #bounds values in R matrix
+imagepath = '/home/ab20/Data/Pichette/halfv2/' #path to save new images
+prereorder = 'ON' #should be on for PF at the moment
+idealmethod = 'Lorentzian' #Lorentzian or Gaussian 
+lam = 0.1 #parameter for regularisation
+optCmethod = 'lsmr' #options are scikit 'Ridge' and 'lsmr'
+optRmethod = 'SLSQP' #options are scipy minimise 'SLSQP' and 'least_sq'
+alpha = 0.05 #bounds values in R matrix
 ######DEFINE FUNCTIONS
 def approx_gaus(x, QE, fwhm, centre):
     sigma = 0.5*fwhm/np.sqrt(np.log(2))
@@ -45,13 +52,20 @@ def approx_gaus(x, QE, fwhm, centre):
 ##    return D.flatten()
 ##    return np.sqrt(np.multiply(D, D))
 def findR(R, C, Aideal, r, rref):
-    R = np.reshape(R, (bandresponses.shape[1]-1, bandresponses.shape[1]-1))
+    R = np.reshape(R, (bandresponses.shape[1]-1, bandresponses.shape[1]-3))
     D = np.dot(np.transpose(C+R), r) - np.dot(np.transpose(Aideal), rref)
     return D.flatten()
 def findRcost(R, C, Aideal, r, rref):
-    R = np.reshape(R, (bandresponses.shape[1]-1, bandresponses.shape[1]-1))
+    R = np.reshape(R, (bandresponses.shape[1]-1, bandresponses.shape[1]-3))
     D = np.dot(np.transpose(C+R), r) - np.dot(np.transpose(Aideal), rref)
     return np.sum(np.square(D))
+def lorentz(x, QE, fwhm, centre):
+    x2 = scipy.constants.c/x
+    fwhm2 = (-scipy.constants.c/centre**2)*fwhm
+    centre2 = scipy.constants.c/centre 
+    y = fwhm2**2/(fwhm2**2 + 4*(x2-centre2)**2)
+    y2 = QE*y
+    return y2
 ######IMPORT DATA
 bandresponses = genfromtxt(path+bands, delimiter=',')
 bandresponses = np.delete(bandresponses, 0, 0)
@@ -72,7 +86,7 @@ plt.plot(sortedlight[:, 0], sortedlight[:, 1], label='original light spectrum')
 plt.show(block=False)
 sortedfilter[:,1] = (sortedfilter[:,1]-sortedfilter[:,1].min())/(sortedfilter[:,1].max()-sortedfilter[:,1].min())
 plt.figure("filter spectrum")
-plt.plot(sortedfilter[:, 0], sortedfilter[:, 1], label='original filter spectrum')
+#plt.plot(sortedfilter[:, 0], sortedfilter[:, 1], label='original filter spectrum')
 #expand filter array to be same shape as light source spectrum 
 interpolatedfilter = np.zeros(sortedlight.shape)
 interpolatedfilter[:,0] = sortedlight[:, 0]
@@ -81,9 +95,10 @@ end = np.array([1200, 1])
 sortedfilter = np.vstack((begin, sortedfilter, end))
 f = interp1d(sortedfilter[:, 0], sortedfilter[:, 1])
 interpolatedfilter[:, 1] = f(interpolatedfilter[:, 0])
-plt.plot(interpolatedfilter[:, 0], interpolatedfilter[:, 1], label='interpolated filter')
+plt.plot(interpolatedfilter[:, 0], interpolatedfilter[:, 1], label='filter spectrum')
 plt.legend(loc='best')
-plt.show(block=False) 
+plt.show(block=False)
+plt.savefig(imagepath+'Filter_Spectrum')
 #multiply light source and filter spectra
 sortedfilter = np.delete(sortedfilter, 0, 0)
 sortedfilter = np.delete(sortedfilter, -1, 0)
@@ -91,15 +106,16 @@ correctedlight = np.zeros(sortedlight.shape)
 correctedlight[:,0] = sortedlight[:, 0]
 correctedlight[:, 1] = np.multiply(sortedlight[:, 1], interpolatedfilter[:,1])
 plt.figure('light spectrum')
-plt.plot(correctedlight[:, 0], correctedlight[:, 1], label='multiplied by filter')
+#plt.plot(correctedlight[:, 0], correctedlight[:, 1], label='multiplied by filter')
 #create less resolved light spectrum to match wavelength axis of all other data
 smalllight = np.zeros((bandresponses.shape[0], 2))
 smalllight[:, 0] = wavelengths
 g = interp1d(correctedlight[:,0], correctedlight[:,1])
 smalllight[:,1] = g(smalllight[:,0])
-plt.plot(smalllight[:,0], smalllight[:,1], label='less resolved spectrum')
+plt.plot(smalllight[:,0], smalllight[:,1], label='multiplied by filter')
 plt.legend(loc = 'best')
 plt.show(block = False)
+plt.savefig(imagepath+'Light_Spectrum')
 ######Creating optical transmission but should measure?
 optictrans = np.ones((bandresponses.shape[0], 1)) 
 noise = np.random.normal(0, 0.5, optictrans.shape)
@@ -173,6 +189,7 @@ for file in sorted(os.listdir(datapath)):
             refdata[i] = np.sum(segmenteddata[:,:,i])/n[i]
         Rdata[:, a:a+1] = refdata
         a = a+1
+spectra = spectra[:, 0:a]
 ######GENERATING IDEALS
 optictransideal = np.ones((bandresponses.shape[0], 1)) #all 1 as ideally no loss of intensity
 lightideal = np.copy(wavelengths)
@@ -182,15 +199,22 @@ lightideal = np.where(lightideal!=0, 1, lightideal) #ideally box function where 
 plt.figure('band responses')
 ##Approximate Gaussians for band responses
 bandideal = np.zeros((bandresponses.shape[0], bandresponses.shape[1]-1))
+cmap = plt.cm.Greys
+colors = [cmap(i) for i in range(cmap.N)]
+cmap2 = plt.cm.Greens
+colors2 = [cmap2(i) for i in range(cmap2.N)]
 for i in range(bandresponses.shape[1]-1):
-    bandideal[:, i] = approx_gaus(x=wavelengths, QE=bandparameters[2, i], fwhm=bandparameters[1, i], centre=bandparameters[0, i])
+    if idealmethod == 'Gaussian':
+        bandideal[:, i] = approx_gaus(x=wavelengths, QE=bandparameters[2, i], fwhm=bandparameters[1, i], centre=bandparameters[0, i])
 ##    plt.figure(i+1)
-    if i == 0: 
-        plt.plot(wavelengths, bandresponses[:, i+1], color='k',  label='real')
-        plt.plot(wavelengths, bandideal[:, i], color = 'r', label = 'ideal')
+    if idealmethod == 'Lorentzian':
+        bandideal[:, i] = lorentz(x=wavelengths, QE=bandparameters[2, i], fwhm=bandparameters[1, i], centre=bandparameters[0, i]) 
+    plt.plot(wavelengths, bandresponses[:, i+1], color=colors[-i*7],  label='real')
+    plt.plot(wavelengths, bandideal[:, i], color = colors2[-i*7], label = 'ideal')
     if i ==0:
         plt.legend(loc='best')
     plt.show(block=False)
+    plt.savefig(imagepath+'Band_responses')
 #######GENERATING A AND Aideal MATRICES
 #may need to reshape all wavelength axes similarly to light source x filter calc
 A = np.zeros(bandideal.shape)
@@ -201,32 +225,70 @@ for i in range(bandresponses.shape[1]-1):
 for i in range(bandresponses.shape[1]-1):
     A[:, i] = A[:, i]/A.sum(axis=0)[i]
     Aideal[:, i] = Aideal[:, i]/Aideal.sum(axis=0)[i]
+Aideal = np.delete(Aideal, 20, 1)
+Aideal = np.delete(Aideal, 21, 1)
 #######FIND C BY MINIMISATION WITH REGULARISATION
 ##C, flag = leastsq(findC, x0 = np.ones(((bandresponses.shape[1]-1)**2, 1)), args=(A, Aideal))
 ##C = np.reshape(C, (bandresponses.shape[1]-1, bandresponses.shape[1]-1))
 ##regC, regflag = leastsq(regfindC, x0 = np.ones(((bandresponses.shape[1]-1)**2, 1)), args=(A, Aideal, 0))
-regC = Ridge(alpha = lam)
-regC.fit(A, Aideal)
-regCresult = regC.coef_
+if optCmethod == 'Ridge': 
+    regC = Ridge(alpha = lam)
+    regC.fit(A, Aideal)
+    regCresult = regC.coef_
+    regCstats = regC.score(A, Aideal)
+    print(regCstats)
+    regCresult = np.transpose(regCresult)
+if optCmethod == 'lsmr':
+    C = np.zeros((bandresponses.shape[1]-1, Aideal.shape[1]))
+    resid = np.zeros((Aideal.shape[1], 1))
+    for i in range(Aideal.shape[1]):
+        a,b,c,d = lsmr(A, Aideal[:, i], damp = lam)[:4]
+        C[:, i] = a
+        regCresult = C
+        resid[i] = d
+    #print(resid)
+    print('Sum of resid of rows of C = ' + str(resid.sum()))
 #regCresult should be same as C when lam=0 but is transpose probably because of all flattening and reshaping
 ##print(regC)
 ##F = C-Ctest
 ##G = C - regC
 #######FIND R BY REFINEMENT
-a = (-alpha, alpha)
-bounds = ((a, )*((bandresponses.shape[1]-1)**2))
 ##R, flag = leastsq(findR, x0 = np.ones(((bandresponses.shape[1]-1)**2, 1)), args=(regCresult, Aideal, Rdata, spectra))
 #originally used leastsq but not able to bound values so use SLSQP now which gives different results with same input but still gives mostly good fit
-resR = minimize(findRcost, x0 = np.ones(((bandresponses.shape[1]-1)**2, )), bounds=bounds, args=(regCresult, Aideal, Rdata, spectra), method='SLSQP')
-altR = resR.x
-altR = np.reshape(altR, (bandresponses.shape[1]-1, bandresponses.shape[1]-1))
-altR = np.transpose(altR)
+if optRmethod == 'SLSQP':
+    a = (-alpha, alpha)
+    bounds = ((a, )*((bandresponses.shape[1]-1)*(bandresponses.shape[1]-3)))
+    resR = minimize(findRcost, x0 = np.zeros(((bandresponses.shape[1]-1)*(bandresponses.shape[1]-3), )), bounds=bounds, args=(regCresult, Aideal, Rdata, spectra), method='SLSQP', options={'disp': True})
+    altR = resR.x
+    altR = np.reshape(altR, (bandresponses.shape[1]-1, bandresponses.shape[1]-3))
+if optRmethod == 'least_sq':
+    residR = np.zeros((Aideal.shape[1]))
+    R = np.zeros((bandresponses.shape[1]-1, Aideal.shape[1]))
+    for i in range(Aideal.shape[1]):
+        res = lsq_linear(A, Aideal[:, i], bounds = (-alpha, alpha), verbose = 1) #not sure what to put for A and Aideal
+        R[:, i] = res.x
+        residR[i] = res.cost
 ##R, flag = leastsq(findR, x0 = np.ones((regCresult.shape)), args=(regCresult, Aideal, Rdata, spectra))
 ##R = np.reshape(R, (bandresponses.shape[1]-1, bandresponses.shape[1]-1))
 ##R = np.transpose(R)
 regCresult = np.transpose(regCresult)
+altR = np.transpose(altR)
 CR = regCresult + altR
+##X = R - altR
+plt.figure('C+R')
+figCR, (ax1, ax2, ax3) = plt.subplots(1, 3)
+ax1.title.set_text('C')
+ax1.imshow(regCresult, cmap='Blues')
+##ax1.imshow(R, cmap='Blues')
+ax2.title.set_text('R')
+ax2.imshow(altR, cmap='Reds')
+ax3.title.set_text('C+R')
+ax3.imshow(CR, cmap='Purples')
+figCR.show()
+name = 'C+R_lambda=' + str(lam) + '_alpha=' + str(alpha)
+figCR.savefig(str(imagepath)+name+'.png')
 Array = pd.DataFrame(CR)
-Array.to_csv(newpath+newname+'.csv', index=False)
+Array.to_csv(newpath+name+'.csv', index=False)
 Array2 = pd.DataFrame(bandparameters[0, :])
+Array2 = Array2.drop([20,21])
 Array2.to_csv(newpath+newxname+'.csv', index=False)
